b7956cea19d53307c73cdf6a4ce7ab42502f6fc5
Gilberto Medrano <gmedrano@gmail.com>
2012-12-29 17:47:56 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopifytemple::Application.config.secret_token = 'fb506a11e10a7c4dbafd6b3ce56cc3d5522c1741248f9b9bc5f497c66bb454ce304d275f94cde3cf4268b858626b4e4b176f13773de41672923ca37d1fdaf9dc'
